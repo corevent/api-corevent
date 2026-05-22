875142441bda813f016668be58a193946ff3632e
GaMa404 <luzettileonardo@gmail.com>
2026-05-21 23:48:01 -0300

    fix(data-source): apply db url option

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,21 +1,36 @@
 import 'reflect-metadata'
 import { join } from 'path'
 import { config } from 'dotenv'
-import { DataSource } from 'typeorm'
+import { DataSource, DataSourceOptions } from 'typeorm'
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
 import { entities } from '~/database/entities'
 
 config()
 
-export default new DataSource({
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT ?? '5432'),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const shared = {
   entities,
   migrations: [join(__dirname, 'migrations', '*.{ts,js}')],
   namingStrategy: new SnakeNamingStrategy(),
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
-})
+} satisfies Partial<DataSourceOptions>
+
+const dbUrl = process.env.DB_URL
+
+const options: DataSourceOptions = dbUrl
+  ? {
+      type: 'postgres',
+      url: dbUrl,
+      ssl: { rejectUnauthorized: false },
+      ...shared,
+    }
+  : {
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT ?? '5432'),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+      ...shared,
+    }
+
+export default new DataSource(options)
