0a146940c6db1356b2cab084cd68ee381e9f44ef
GaMa404 <luzettileonardo@gmail.com>
2026-05-25 20:55:34 -0300

    feat(attractions): finish attractions module

--- src/modules/attractions/dto/attractions.dto.ts
+++ src/modules/attractions/dto/attractions.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty, OmitType, PartialType } from '@nestjs/swagger'
-import { IsString, IsNotEmpty, IsDate } from 'class-validator'
-import { PaginationMetaDto } from '~/common/pagination/pagination.dto'
+import { IsString, IsNotEmpty, IsDate, IsOptional } from 'class-validator'
+import { PaginationMetaDto, QueryPaginationDto } from '~/common/pagination/pagination.dto'
 
 export class CreateAttractionDto {
   @ApiProperty({ description: 'Attraction title', example: 'Attraction Title' })
@@ -33,16 +33,38 @@ export class AttractionDataDto extends CreateAttractionDto {
 }
 
 export class AttractionResponseDto {
-  @ApiProperty({ description: 'Attractions', type: [AttractionDataDto] })
-  attractions: AttractionDataDto
+  @ApiProperty({ description: 'Data of the attraction', type: AttractionDataDto })
+  data: AttractionDataDto
 }
 
 export class AttractionListDto extends OmitType(AttractionDataDto, ['eventId']) {}
 
 export class PaginatedAttractionsDto {
-  @ApiProperty({ description: 'Attractions', type: [AttractionListDto] })
-  attractions: AttractionListDto[]
+  @ApiProperty({ description: 'List of attractions', type: [AttractionListDto] })
+  data: AttractionListDto[]
 
   @ApiProperty({ description: 'Pagination meta', type: PaginationMetaDto })
   meta: PaginationMetaDto
+}
+
+export class QueryAttractionsDto extends QueryPaginationDto {
+  @ApiProperty({ description: 'Search by title', example: 'Attraction Title', required: false })
+  @IsString()
+  @IsOptional()
+  search?: string
+
+  @ApiProperty({ description: 'Filter by guest', example: 'Attraction Guest', required: false })
+  @IsString()
+  @IsOptional()
+  guest?: string
+
+  @ApiProperty({ description: 'Filter by start date', example: '2026-01-01T00:00:00.000Z', required: false })
+  @IsDate()
+  @IsOptional()
+  startDate?: Date
+
+  @ApiProperty({ description: 'Filter by end date', example: '2026-01-01T00:00:00.000Z', required: false })
+  @IsDate()
+  @IsOptional()
+  endDate?: Date
 }
