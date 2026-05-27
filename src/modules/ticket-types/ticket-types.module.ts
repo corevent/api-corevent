a71c0ab7b4086de0ac7826ee2b9c83bb689614fa
GaMa404 <luzettileonardo@gmail.com>
2026-05-27 14:34:19 -0300

    fix(ticket-types): add events module to ticket types module
    - Add events module to ticket types module to use the events service
    - Implement database connection using DB_URL environment variable for datasource

--- src/modules/ticket-types/ticket-types.module.ts
+++ src/modules/ticket-types/ticket-types.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { TicketTypes } from '~/modules/ticket-types/ticket-types.entity'
 import { TicketTypesService } from '~/modules/ticket-types/ticket-types.service'
 import { TicketTypesController } from '~/modules/ticket-types/ticket-types.controller'
+import { EventsModule } from '../events-module/events.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TicketTypes])],
+  imports: [TypeOrmModule.forFeature([TicketTypes]), EventsModule],
   controllers: [TicketTypesController],
   providers: [TicketTypesService],
   exports: [TicketTypesService],
